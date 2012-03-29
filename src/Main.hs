f1473bf7b36c40b845aa5a08b6fea114fb5ffc86
Ulf Lilleengen <lulf@pvv.ntnu.no>
2012-03-29 19:35:27 +0200

    - Add main pages.

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,10 @@
-{-# LANGUAGE OverloadedStrings #-}
-module Main where
-
-import           Control.Applicative
-import           Snap.Core
-import           Snap.Util.FileServe
-import           Snap.Http.Server
-
 main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
 site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
+    ifTop (writeBS mainPage) <|>
+    route [ ("insert", writeBS "Inserting wish")
           , ("echo/:echoparam", echoHandler)
           ] <|>
     dir "static" (serveDirectory ".")
@@ -22,3 +14,4 @@ echoHandler = do
     param <- getParam "echoparam"
     maybe (writeBS "must specify echo/param in URL")
           writeBS param
+

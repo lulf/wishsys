49442126176c419b214ab02792b145fdc169b876
Ulf Lilleengen <lulf@pvv.ntnu.no>
2012-03-29 21:43:51 +0200

    - We can read parameters and print them!

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,8 @@ import           Control.Applicative
 import           Snap.Core
 import           Snap.Util.FileServe
 import           Snap.Http.Server
+import           Data.Maybe
+import qualified Data.ByteString as B
 
 main :: IO ()
 main = quickHttpServe site
@@ -12,14 +14,16 @@ main = quickHttpServe site
 site :: Snap ()
 site =
     ifTop (serveFile "static/index.html") <|>
-    route [ ("insert", writeBS "Inserting wish")
-          , ("echo/:echoparam", echoHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
+    route [ ("insert", insertHandler)
+          , ("admin", serveFile "static/admin.html")
+          ]
 
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
-
+insertHandler :: Snap ()
+insertHandler = do
+    what <- getPostParam "what"
+    amount <- getPostParam "amount"
+    if what == Nothing
+       then (writeBS "must specify 'what'")
+       else if amount == Nothing
+               then (writeBS "must specify 'amount'")
+               else (writeBS (B.concat ["What: '", (fromJust what), "', Amount: '", (fromJust amount), "'"]))

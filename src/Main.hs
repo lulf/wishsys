741fc300078ffeff01c389aef9a32ceaa7d1d388
Ulf Lilleengen <lulf@pvv.ntnu.no>
2012-03-30 20:54:41 +0200

    - Fix dependencies on hdbc.

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,50 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Control.Applicative
-import           Snap.Core
-import           Snap.Util.FileServe
-import           Snap.Http.Server
-import           Data.Maybe
-import qualified Data.ByteString as B
+import            Control.Monad
+import            Control.Monad.State
+import            Data.ByteString.Char8 (ByteString)
+import qualified  Data.ByteString.Char8 as BS
+import            Data.Lens.Template
+import            Data.Map ((!))
+import            Data.Maybe
+import            Data.String
+-- import            Database.HDBC.Sqlite3
+import            Snap
+import            Snap.Core
+import            Snap.Snaplet
+import            Snap.Snaplet.Auth
+import            Snap.Snaplet.Auth.Backends.Hdbc
+import            Snap.Snaplet.Hdbc
+import            Snap.Snaplet.Session
+import            Snap.Snaplet.Session.Backends.CookieSession
+import            Snap.Util.FileServe
+import            Data.Maybe
+import qualified  Data.ByteString as B
+
+data App = App
+--   { _authLens :: Snaplet (AuthManager App)
+--   , _sessLens :: Snaplet SessionManager
+--   , _dbLens   :: Snaplet (HdbcSnaplet Connection IO)
+--   }
+
+makeLenses [''App]
+
+appInit :: SnapletInit App App
+appInit = makeSnaplet "wishsys" "Wish list application" Nothing $ do
+    addRoutes [ ("", serveFile "static/index.html")
+              , ("insert", insertHandler)
+              , ("admin", serveFile "static/admin.html") ]
+    return $ App
 
 main :: IO ()
-main = quickHttpServe site
+main = serveSnaplet defaultConfig appInit
 
-site :: Snap ()
-site =
-    ifTop (serveFile "static/index.html") <|>
-    route [ ("insert", insertHandler)
-          , ("admin", serveFile "static/admin.html")
-          ]
-
-insertHandler :: Snap ()
+insertHandler :: Handler b v ()
 insertHandler = do
-    what <- getPostParam "what"
-    amount <- getPostParam "amount"
+    what <- getParam "what"
+    amount <- getParam "amount"
     if what == Nothing
        then (writeBS "must specify 'what'")
        else if amount == Nothing

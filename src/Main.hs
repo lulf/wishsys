0a3d21f18fc131a584d966dd4923dfe52f54a926
Ulf Lilleengen <lulf@pvv.ntnu.no>
2012-03-30 23:18:22 +0200

    - Add Wish data type and function for looking them up from a database.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Main where
 
 import            Control.Monad
@@ -10,7 +12,7 @@ import            Data.Lens.Template
 import            Data.Map ((!))
 import            Data.Maybe
 import            Data.String
--- import            Database.HDBC.Sqlite3
+import            Database.HDBC.Sqlite3
 import            Snap
 import            Snap.Core
 import            Snap.Snaplet
@@ -24,10 +26,10 @@ import            Data.Maybe
 import qualified  Data.ByteString as B
 
 data App = App
---   { _authLens :: Snaplet (AuthManager App)
+   { --_authLens :: Snaplet (AuthManager App)
 --   , _sessLens :: Snaplet SessionManager
---   , _dbLens   :: Snaplet (HdbcSnaplet Connection IO)
---   }
+     _dbLens   :: Snaplet (HdbcSnaplet Connection IO)
+   }
 
 makeLenses [''App]
 
@@ -36,12 +38,14 @@ appInit = makeSnaplet "wishsys" "Wish list application" Nothing $ do
     addRoutes [ ("", serveFile "static/index.html")
               , ("insert", insertHandler)
               , ("admin", serveFile "static/admin.html") ]
-    return $ App
+    let sqli = connectSqlite3 "test/test.db"
+    _dblens'  <- nestSnaplet "hdbc" dbLens $ hdbcInit sqli
+    return $ App _dblens'
 
 main :: IO ()
 main = serveSnaplet defaultConfig appInit
 
-insertHandler :: Handler b v ()
+insertHandler :: Handler App App ()
 insertHandler = do
     what <- getParam "what"
     amount <- getParam "amount"
@@ -50,3 +54,16 @@ insertHandler = do
        else if amount == Nothing
                then (writeBS "must specify 'amount'")
                else (writeBS (B.concat ["What: '", (fromJust what), "', Amount: '", (fromJust amount), "'"]))
+
+data Wish = Wish String Integer deriving (Show)
+
+getWishes :: HasHdbc m c s => m [Wish]
+getWishes = do
+    rows <- query "SELECT * FROM list" []
+    return $ map toWish rows
+    where toWish :: Row -> Wish
+          toWish rw = Wish (fromSql (rw ! "item")) (fromSql (rw ! "amount"))
+
+
+instance HasHdbc (Handler App App) Connection IO where
+    getHdbcState = with dbLens get

91e373702f6896c15eb4f06c67097f5188121a21
Ulf Lilleengen <ulf_lilleengen@yahoo.com>
2013-05-12 16:15:15 +0200

    Tabularize

--- src/Config.hs
+++ src/Config.hs
@@ -11,24 +11,24 @@ import Data.List.Split
 data WishsysConfig = WishsysConfig {
   guestUsers :: [String],
   adminUsers :: [String],
-  wishDB :: String,
-  siteKey :: String,
-  userDB :: String
+  wishDB     :: String,
+  siteKey    :: String,
+  userDB     :: String
 } deriving (Show)
 
 fromConfigFile :: String -> IO WishsysConfig
 fromConfigFile fileName = do
   cf <- readfile emptyCP fileName :: IO (Either CPError ConfigParser)
-  let cp = forceEither cf
-  let wishdb = (forceEither $ get cp "DEFAULT" "wish_db") :: String
-  let userdb = (forceEither $ get cp "DEFAULT" "user_db") :: String
+  let cp      = forceEither cf
+  let wishdb  = (forceEither $ get cp "DEFAULT" "wish_db") :: String
+  let userdb  = (forceEither $ get cp "DEFAULT" "user_db") :: String
   let sitekey = (forceEither $ get cp "DEFAULT" "site_key") :: String
-  let guests = (forceEither $ get cp "DEFAULT" "guest_users") :: String
-  let admins = (forceEither $ get cp "DEFAULT" "admin_users") :: String
-  let config = WishsysConfig (splitOn "," guests)
-                             (splitOn "," admins)
-                             wishdb
-                             sitekey
-                             userdb
+  let guests  = (forceEither $ get cp "DEFAULT" "guest_users") :: String
+  let admins  = (forceEither $ get cp "DEFAULT" "admin_users") :: String
+  let config  = WishsysConfig (splitOn "," guests)
+                              (splitOn "," admins)
+                              wishdb
+                              sitekey
+                              userdb
   putStrLn $ "String up with following config: " ++ (show config)
   return $ config

a1f42881d42586a8f180143f4bb6a038ef57c27a
Ulf Lilleengen <ulf_lilleengen@yahoo.com>
2013-06-02 21:08:40 +0200

    Guests are authorized to wish lists as well

--- Foundation.hs
+++ Foundation.hs
@@ -104,7 +104,9 @@ instance Yesod App where
         case mauth of
             Nothing -> return AuthenticationRequired
             Just (Entity userid _) -> runDB $ do
-                                        wishList <- selectList [WishlistId ==. id, WishlistOwner ==. userid] []
+                                        wishList <- selectList ([WishlistId ==.  id, WishlistOwner ==. userid] ||.
+                                                                [WishlistId ==.  id, WishlistGuest ==. userid])
+                                                               []
                                         case wishList of
                                             [] -> return $ Unauthorized "You do not have permission to view this wish list"
                                             _ -> return Authorized

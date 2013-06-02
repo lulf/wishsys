1c23e11b17c2be521af88960f7f69c1a83671c06
Ulf Lilleengen <ulf_lilleengen@yahoo.com>
2013-06-02 18:57:44 +0200

    Make auth login with guest and admin users work

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,9 +2,11 @@
 module Handler.Home where
 
 import Yesod.Auth
+import Yesod.Auth.HashDB (validateUser)
 import Import
-import Data.Text (pack)
+import Data.Text (pack, unpack)
 import Control.Arrow
+import Data.Maybe
 
 data AccessLevel = Guest | Admin
     deriving (Show, Eq, Enum, Bounded)
@@ -19,11 +21,37 @@ getHomeR = do
 
 postHomeR :: Handler RepHtml
 postHomeR = do
-    ((result, formWidget), enctype) <- runFormPost guestForm
-    defaultLayout $ do
-        aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
+    ((result, _), _) <- runFormPost guestForm
+    case result of 
+        FormSuccess (name, password, accessLevel) -> do
+            let loginName = case accessLevel of
+                                Admin -> pack $ "admin_" ++ (unpack name)
+                                Guest -> pack $ "guest_" ++ (unpack name)
+            doLogin loginName password
+            wl <- runDB $ selectList [WishlistName ==. name] [LimitTo 1]
+            case wl of
+                [] -> do
+                    setMessage "Wish list not found"
+                    redirect $ HomeR
+                (Entity wid _):_ -> do
+                    redirect $ (WishListR wid)
+        _ -> do
+            setMessage "Error login in" -- i18n
+            redirect $ HomeR
+
+
+doLogin :: Text -> Text -> Handler ()
+doLogin mu mp = do
+    let uid = Just $ UniqueUser mu 
+    isValid <- fromMaybe (return False) (validateUser <$> uid <*> (Just mp))
+    if isValid 
+       then setCreds True $ Creds "hashdb" mu []
+       else loginErrorMessage (AuthR LoginR) "Invalid username/password"
+    --defaultLayout $ do
+    --    aDomId <- newIdent
+    --    setTitle "Welcome To Yesod!"
+    --    $(widgetFile "homepage")
+            
 
 guestForm :: Form (Text, Text, AccessLevel)
 guestForm = renderBootstrap $ (,,)

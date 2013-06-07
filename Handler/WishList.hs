e82d4b5ad3bcdf22e0351d540046cfa282900129
Ulf Lilleengen <ulf_lilleengen@yahoo.com>
2013-06-07 17:16:28 +0200

    Add separation between guest and admin, but give same view for now.

--- Handler/WishList.hs
+++ Handler/WishList.hs
@@ -1,14 +1,26 @@
 {-# LANGUAGE TupleSections, OverloadedStrings #-}
 module Handler.WishList where
 
-
 import Import
+import Yesod.Auth
 
 getWishListR :: WishlistId -> Handler RepHtml
-getWishListR _ = do
-    wishes <- runDB $ selectList ([] :: [Filter Wish]) []
+getWishListR listId = do
+    list <- runDB $ get listId
+    wishes <- runDB $ selectList ([WishWlist ==. listId] :: [Filter Wish]) []
+    userId <- requireAuthId
     let numWishes = length wishes
     let handlerName = "getWishListR" :: Text
+    let widget = getWidget list wishes userId
     defaultLayout $ do
-        setTitleI MsgWishListTitle
-        $(widgetFile "wishlist")
+      setTitleI MsgWishListTitle
+      widget
+
+getWidget :: Maybe Wishlist -> [Entity Wish] -> UserId -> Widget
+getWidget Nothing _ _ = $(widgetFile "impossible")
+getWidget (Just (Wishlist _ ownerId guestId)) wishes userId =
+  if userId == guestId
+  then $(widgetFile "wishlist_guest")
+  else if userId == ownerId
+       then $(widgetFile "wishlist_owner")
+       else $(widgetFile "impossible")

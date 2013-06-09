b4e9be1ca678c2621bedbf4fc37f210e90dd2f00
Ulf Lilleengen <ulf_lilleengen@yahoo.com>
2013-06-09 17:13:00 +0200

    Add logout button if logged out

--- Foundation.hs
+++ Foundation.hs
@@ -75,6 +75,7 @@ instance Yesod App where
         master <- getYesod
         mmsg <- getMessage
 
+        maid <- maybeAuthId 
         -- We break up the default layout into two components:
         -- default-layout is the contents of the body tag, and
         -- default-layout-wrapper is the entire page. Since the final

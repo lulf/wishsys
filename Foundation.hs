e978f3cc71da6e71d525b1f8afb24de265b5ca07
Ulf Lilleengen <ulf_lilleengen@yahoo.com>
2013-06-16 17:11:48 +0200

    set default to english

--- Foundation.hs
+++ Foundation.hs
@@ -35,7 +35,7 @@ data App = App
     }
 
 -- Set up i18n messages. See the message folder.
-mkMessage "App" "messages" "no"
+mkMessage "App" "messages" "en"
 
 -- This is where we define all of the routes in our application. For a full
 -- explanation of the syntax, please see:

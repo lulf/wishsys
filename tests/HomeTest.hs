d22a96add41b3d3223ab6913d8bf82aa365bd37f
Ulf Lilleengen <ulf_lilleengen@yahoo.com>
2013-06-24 19:57:36 +0200

    add home test

--- tests/HomeTest.hs
+++ tests/HomeTest.hs
@@ -8,25 +8,14 @@ import qualified Data.List as L
 
 homeSpecs :: Specs
 homeSpecs =
-    ydescribe "These are some example tests" $ do
+    ydescribe "The home page" $ do
 
-        yit "loads the index and checks it looks right" $ do
+        yit "displays the login form" $ do
             get HomeR
             statusIs 200
-            htmlAllContain "h1" "Hello"
-
-            request $ do
-                setMethod "POST"
-                setUrl HomeR
-                addNonce
-                fileByLabel "Choose a file" "tests/main.hs" "text/plain" -- talk about self-reference
-                byLabel "What's on the file?" "Some Content"
-
-            statusIs 200
+            htmlAllContain "h1" "Wishsys"
             printBody
-            htmlCount ".message" 1
-            htmlAllContain ".message" "Some Content"
-            htmlAllContain ".message" "text/plain"
+            bodyContains "Welcome to wishsys"
 
         -- This is a simple example of using a database access in a test.  The
         -- test will succeed for a fresh scaffolded site with an empty database,

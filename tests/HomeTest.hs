2242bd55789aa0f24a45faad88bd4b35d2926570
Ulf Lilleengen <ulf_lilleengen@yahoo.com>
2013-06-24 20:57:06 +0200

    more tests.

--- tests/HomeTest.hs
+++ tests/HomeTest.hs
@@ -10,18 +10,10 @@ homeSpecs :: Specs
 homeSpecs =
     ydescribe "The home page" $ do
 
-        yit "displays the login form" $ do
+        yit "displays the correct elements" $ do
             get HomeR
             statusIs 200
             htmlAllContain "h1" "Wishsys"
-            printBody
             bodyContains "Welcome to wishsys"
-
-        -- This is a simple example of using a database access in a test.  The
-        -- test will succeed for a fresh scaffolded site with an empty database,
-        -- but will fail on an existing database with a non-empty user table.
-        yit "leaves the user table empty" $ do
-            get HomeR
-            statusIs 200
-            users <- runDB $ selectList ([] :: [Filter User]) []
-            assertEqual "user table empty" 0 $ L.length users
+            bodyContains "Copyright Ulf Lilleengen"
+            bodyContains "checked>Gjest"

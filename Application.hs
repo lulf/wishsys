14885d67cfae8c4ca7f04072d0f491e4fff37537
Ulf Lilleengen <ulf_lilleengen@yahoo.com>
2013-06-25 19:49:31 +0200

    switch to sqlite database backend, as postgres is not yet needed

--- Application.hs
+++ Application.hs
@@ -58,7 +58,7 @@ makeFoundation :: AppConfig DefaultEnv Extra -> IO App
 makeFoundation conf = do
     manager <- newManager def
     s <- staticSite
-    dbconf <- withYamlEnvironment "config/postgresql.yml" (appEnv conf)
+    dbconf <- withYamlEnvironment "config/sqlite.yml" (appEnv conf)
               Database.Persist.loadConfig >>=
               Database.Persist.applyEnv
     p <- Database.Persist.createPoolConfig (dbconf :: Settings.PersistConf)

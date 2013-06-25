4071e372b193dba3f3e95ec24668ec96ec3f57bb
Ulf Lilleengen <ulf_lilleengen@yahoo.com>
2013-06-25 20:33:02 +0200

    clean up database before running test test that wishes are persisted to disk

--- tests/RegisterTest.hs
+++ tests/RegisterTest.hs
@@ -6,14 +6,20 @@ module RegisterTest
 import TestImport
 import qualified Data.List as L
 
+cleanDB :: YesodExample App ()
+cleanDB = do
+    runDB $ deleteWhere ([] :: [Filter Wish])
+    runDB $ deleteWhere ([] :: [Filter Wishlist])
+    runDB $ deleteWhere ([] :: [Filter User])
+
 registerSpecs :: Specs
 registerSpecs =
     ydescribe "The register page" $ do
 
         yit "displays the correct elements" $ do
+            cleanDB
             get RegisterR
             statusIs 200
-            printBody
             htmlAllContain "h1" "Create new wish list"
             bodyContains "Copyright Ulf Lilleengen"
             bodyContains "Name of wish list"
@@ -21,5 +27,16 @@ registerSpecs =
             bodyContains "Guest password"
 
         yit "registers new lists through POST requests" $ do
+            cleanDB
             get RegisterR
-            statusIs 200
+            request $ do
+                setMethod "POST"
+                setUrl RegisterR
+                addNonce
+                byLabel "Name of wish list" "foobar"
+                byLabel "Administrator password" "foo"
+                byLabel "Guest password" "bar"
+
+            statusIs 303
+            listAfter <- runDB $ selectList ([] :: [Filter Wishlist]) []
+            assertEqual "wish list was not registered!" 1 $ L.length listAfter

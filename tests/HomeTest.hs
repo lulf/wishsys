ecad10a0bb2488c4d1437c1faa10a7a29fd19c12
Ulf Lilleengen <ulf_lilleengen@yahoo.com>
2013-07-14 17:50:56 +0200

    use languate agnostic rendering in tests

--- tests/HomeTest.hs
+++ tests/HomeTest.hs
@@ -5,14 +5,22 @@ module HomeTest
 
 import TestImport
 import qualified Data.List as L
+import           Text.Shakespeare.I18N         (RenderMessage (..))
+import qualified Data.Text as T
+
+
+getMessageRender = do
+    y <- getTestYesod
+    return $ renderMessage y ["en"]
 
 homeSpecs :: Specs
 homeSpecs =
     ydescribe "The home page" $ do
 
         yit "displays the correct elements" $ do
+            render <- getMessageRender
             get HomeR
             statusIs 200
-            htmlAnyContain "h1" "Wishsys"
-            bodyContains "Welcome to wishsys"
+            htmlAnyContain "h1" $ T.unpack $ render MsgHomeHeader
+            bodyContains $ T.unpack $ render MsgHomeWelcome
             bodyContains "Lilleengen Programvarefabrikk"

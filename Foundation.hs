441e0c475975cfb3cb4edd7f8ddf1ae45f13bd8f
Ulf Lilleengen <lulf@yahoo-inc.com>
2013-09-16 08:18:32 +0200

    Remove unused code.

--- Foundation.hs
+++ Foundation.hs
@@ -187,12 +187,6 @@ instance YesodAuth App where
     logoutDest _ = HomeR
 
     getAuthId creds = getAuthIdHashDB AuthR (Just . UniqueUser) creds
-    --getAuthId creds = runDB $ do
-    --    x <- getBy $ UniqueEmail $ credsIdent creds
-    --    case x of
-    --        Just (Entity uid _) -> return $ Just uid
-    --        Nothing -> do
-    --            fmap Just $ insert $ User (credsIdent creds) Nothing
 
     authPlugins _ = [authHashDB (Just . UniqueUser)]
 
